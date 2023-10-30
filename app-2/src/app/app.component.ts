103d37ad4f1608ad5bced84f1339f72fdf8ee409
mylo <milopadma@yahoo.com>
2023-10-30 14:43:22 +0800

    routing

--- app-2/src/app/app.component.ts
+++ app-2/src/app/app.component.ts
@@ -12,6 +12,27 @@ import { Component, OnInit } from '@angular/core';
       >
         <button (click)="change()">change</button>
       </div>
+      <!-- generic login form -->
+      <div class="h-8"></div>
+      <div
+        class="flex flex-col items-center justify-center border-2 border-sky-400 rounded-lg px-4 py-2"
+      >
+        <form action="submit">
+          <div class="flex flex-col">
+            <label for="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div class="h-4"></div>
+          <div class="flex flex-col">
+            <label for="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div class="h-4"></div>
+          <div class="flex flex-col">
+            <button type="submit" (click)="onSubmit()">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   `,
 })
@@ -25,5 +46,11 @@ export class AppComponent implements OnInit {
   change() {
     // change title
     this.title = 'app-2 which two';
+  }
+
+  // on form submit, send data to server
+  onSubmit() {
+    // send data to server
+    console.log('onSubmit');
   }
 }
